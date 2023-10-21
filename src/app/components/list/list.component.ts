351c1d998b3d30a847c6282c7be6a135feed72ec
Luis Carrillo <luis.carrilloo.perez@gmail.com>
2023-10-21 13:21:51 -0600

    [ADD] disable button

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -3,21 +3,23 @@ import { CommonModule } from '@angular/common';
 import { TaskComponent } from '../task/task.component';
 import { Store } from '@ngrx/store';
 import { addTask, removeAllTasks, resetAllTasks, selectTaks } from 'src/app/store/task.store';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CommonModule, TaskComponent],
+  imports: [CommonModule, TaskComponent, ReactiveFormsModule],
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent {
   store = inject(Store)
   tasks = this.store.selectSignal(selectTaks)
-
+  formCtrl = new FormControl(null, [Validators.required, Validators.minLength(5)])
 
   addTask():void{
-    this.store.dispatch(addTask({ title: 'HEYHEY' }))
+    this.store.dispatch(addTask({ title: this.formCtrl.value ?? '' }))
+    this.formCtrl.setValue(null)
   }
 
   clearTasks():void {
